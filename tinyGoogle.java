67e903fd5737cba4a0d6eb7fd16358ebb69feb66
Jacob Hershey <jah215@pitt.edu>
2017-03-27 12:13:43 -0400

    added rank to tinyGoogle, need updated wordcount to test

--- tinyGoogle.java
+++ tinyGoogle.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 import java.util.StringTokenizer;
 import java.util.Scanner;
 
-/*import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
@@ -10,22 +10,25 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;*/
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class tinyGoogle {
-	/*public static class TokenizerMapper
+	String[] queryInput;
+	
+	public static class RankMapper
 		extends Mapper<Object, Text, Text, IntWritable>
 		{
-			private final static IntWritable one = new IntWritable(1);
 			private Text word = new Text();
 
 			public void map(Object key, Text value, Context context) throws IOException, InterruptedException 
 			{
-				StringTokenizer itr = new StringTokenizer(value.toString());
-				while (itr.hasMoreTokens())
+				String[] itr = value.toString.split(",");
+				if(Arrays.asList(queryInput).contains(itr[0]))
 				{
-					word.set(itr.nextToken());
-					context.write(word, one);
+					String[] tmp = itr[1].split(",");
+					word.set(tmp[0]);
+					private IntWritable numOccurences = new IntWritable(Integer.parseInt(tmp[1]));
+					context.write(word, numOccurences);
 				}
 			}
 		}
@@ -45,7 +48,7 @@ public class tinyGoogle {
 				context.write(key, result);
 			}
 		}
-*/
+
 	public static void main(String[] args) throws Exception 
 	{
 		int userInput = 0;
@@ -55,6 +58,8 @@ public class tinyGoogle {
 			System.out.println("Welcome to tiny-Google.\nEnter 1 to a document to index\nEnter 2 to enter a query to search for in the indexed documents.\nEnter 3 to quit the program.");
 			//Scanner sc = new Scanner(System.in);
 			userInput = sc.nextInt();
+			if(userInput == 2)
+				Rank();
 		} while(userInput != 3);
 		
 		/*Configuration conf = new Configuration();
@@ -68,5 +73,24 @@ public class tinyGoogle {
 		FileInputFormat.addInputPath(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 		System.exit(job.waitForCompletion(true) ? 0 : 1);*/
+	}
+	
+	public static void Rank()
+	{
+		System.out.println("Please enter a query, each keyword seperated by a space.: ");
+		Scanner sc = new Scanner(System.in);
+		queryInput = sc.nextLine().split(" ");
+		//Begin mapreduce section//
+		Configuration conf = new Configuration();
+		Job job = Job.getInstance(conf, "Rank");
+		job.setJarByClass(tinyGoogle.class);
+		job.setMapperClass(RankMapper.class);
+		job.setCombinerClass(IntSumReducer.class);
+		job.setReducerClass(IntSumReducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		FileInputFormat.addInputPath(job, "output");
+		FileOutputFormat.setOutputPath(job, "rankedOutput");
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
 }
