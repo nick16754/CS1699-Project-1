f3e896e4210b062c02252926715448606d413767
Nicholas Taglianetti <Nicholas Taglianetti>
2017-03-27 12:57:06 -0400

    import bug fixed, sample output added

--- InvertedIndex.java
+++ InvertedIndex.java
@@ -11,7 +11,8 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+//import org.apache.hadoop.mapred.*;
 import org.apache.hadoop.util.*;
 
 /**
@@ -31,7 +32,7 @@ public class InvertedIndex {
             line = line.replaceAll("[\\p{P}+~$`^=|<>~'$^+=|<>]", "");//replace all the unrelated symbols
             StringTokenizer itr = new StringTokenizer(line);
             while (itr.hasMoreTokens()) {
-                // write word
+                // as key, write word concatenated to filename with delimiter ":"
                 word.set(itr.nextToken().toLowerCase() + ":" + filename);
                 context.write(word, one);
             }
