c9f5055b61c7fbbe5c4bb7fe1cf9e8dcaae3e240
Jacob Hershey <jah215@pitt.edu>
2017-03-28 12:11:09 -0400

    read file

--- tinyGoogle.java
+++ tinyGoogle.java
@@ -3,7 +3,7 @@ import java.util.StringTokenizer;
 import java.util.Scanner;
 import java.util.HashMap;
 import java.io.BufferedReader;
-java.io.InputStreamReader;
+import java.io.InputStreamReader;
 
 
 import org.apache.hadoop.conf.Configuration;
