656cb1e7cdff5b9979269a6e9946f3170468cc82
Nicholas Taglianetti <Nicholas Taglianetti>
2017-04-26 16:15:46 -0400

    unzipped books and shell script added

---
+++ tinyGoogle.sh
@@ -0,0 +1,10 @@
+# Bash shell script to prompt user for tinyGoogle query
+# Sends query to 'input' and runs tinyGoogle.scala which uses query in 'input' for Rank and Retrieval
+# Written for and tested with Docker sequenceiq/spark-native-yarn image
+# EOL line endings must be Unix (LF)
+#!/bin/bash
+echo "Welcome To tinyGoogle!"
+read -p "Please enter your search query: "
+echo "$REPLY" > input
+echo "Running tinyGoogle with query '$REPLY' in 'input'."
+spark-shell -i tinyGoogle.scala
